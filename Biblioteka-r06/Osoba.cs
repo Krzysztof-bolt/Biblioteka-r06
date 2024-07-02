c72d9b34a0f074592d0149e1a25e4a7b93a5fec7
Krzysztof Wołowski <78325693+Krzysztof-bolt@users.noreply.github.com>
2024-07-02 15:54:37 +0200

    Dodaj pliki projektów.

---
+++ Biblioteka-r06/Osoba.cs
@@ -0,0 +1,14 @@
+﻿namespace BibliotekaWspolna;
+
+    public class Osoba : object 
+    {
+        //właściowści
+        public string? Nazwisko {  get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        //metody
+        public void WypiszWKonsoli()
+        {
+            WriteLine($"{Nazwisko},dzien urodzenia {DataUrodzenia:dddd}.");
+        }
+    }
